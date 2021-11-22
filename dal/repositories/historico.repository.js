0adfaa9c933b0c0a59b0943e2ccb299693f994d9
CristianSteve <ccristiansteve14@gmail.com>
2021-11-22 16:41:12 -0500

    Se arregla consulta del historico

--- dal/repositories/historico.repository.js
+++ dal/repositories/historico.repository.js
@@ -12,22 +12,8 @@ class HistoryRepository extends BaseRepository {
     return this._db.Historico.findAll({where : {createdAt : {[Op.startsWith] : id}}})
   }
   
-  getDateTypeHistory(id, Componente_idComponente) {
-    return this._db.Historico.findAll({where : { [Op.and] : [{createdAt : {[Op.startsWith] : id}}, {Componente_idComponente}]}})
-  } 
-
-  getProductStock(product) {
-/*     return this._db["Stock"].findAll({
-      include: [
-        {
-          model: this._db["Producto"],
-          as: "producto",
-          where: { nombre: { [Op.like]: `%${product}%` } },
-          require: true,
-          right: true,
-        },
-      ],
-    });*/
+  getDateTypeHistory(id, idAcceso) {
+    return this._db.Historico.findAll({where : { [Op.and] : [{createdAt : {[Op.startsWith] : id}}, {idAcceso}]}})
   } 
 }
 
